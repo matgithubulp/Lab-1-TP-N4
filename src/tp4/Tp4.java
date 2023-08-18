98d64293b71a5d7219a760c510325c6e861a8717
matgithubulp <ejarquegithub@gmail.com>
2023-08-18 13:08:07 -0300

    Initial commit

---
+++ src/tp4/Tp4.java
@@ -0,0 +1,14 @@
+package tp4;
+
+public class Tp4 {
+
+    public static void main(String[] args) {
+
+       
+            Temperatura iniciarTemperatura = new Temperatura();
+            iniciarTemperatura.mostrarPanel();
+       
+
+    }
+
+}
